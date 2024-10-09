c9731aa75a544a4f247f6b1463e80a4d79129412
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-09 11:51:00 -0300

    kukyu

--- perguntas.js
+++ perguntas.js
@@ -1,43 +1,99 @@
-1 criaCartão (
-    &#39;Química&#39;, &#39;Qual o símbolo químico do Carbono?&#39;, &#39;C&#39;)
-    2 criaCartão (
-    &#39;Artes&#39;, &#39;Em que ano se deu início ao período barroco?&#39;, &#39;1600&#39;)
-    3 criaCartão (
-    &#39;Matemática&#39;, &#39;Qual a soma dos ângulos de um triângulo?&#39;, &#39;180° graus&#39;)
-    4 criaCartão (
-    &#39;Biologia&#39;, &#39;O que é um ser pluricelular&#39;, &#39;Um ser com mais de uma célula&#39;)
-    5 criaCartão (
-    &#39;História&#39;, &#39;No Brasil, quem foi o presidente que mais ficou no cargo?&#39;, &#39;Getúlio
-    Vargas&#39;)
-    6 criaCartão (
-    &#39;Ed. Física&#39;, &#39;Quando o vôlei foi criado?&#39;, &#39;1895&#39;)
-    7 criaCartão (
-    &#39;Inglês&#39;, &#39;Como se OI em ingles?&#39;, &#39; Hi&#39;)
-    8 criaCartão (
-    &#39;Geografia&#39;, &#39;Qual o maior país do mundo?&#39;, &#39;Russía&#39;)
-    9 criaCartão (
-    &#39;Robótica&#39;, &#39;Para que serve os jumpers?&#39;, &#39;Fazer conexões entre o sistema&#39;)
-    10 criaCartão (
-    &#39;Programação&#39;, &#39;Diga um site que usamos para programar sites&#39;, &#39;Github&#39;)
-    11 criaCartão (
-    &#39;Filosofia&#39;, &#39;Diga um filósofo conhecido&#39;, &#39;Aristóteles&#39;)
-    12 criaCartão (
-    
-    &#39;Sociologia&#39;, &#39;Quem que é o pai da sociologia?&#39;, &#39;Auguste Comte&#39;)
-    13 criaCartão (
-    &#39;Biologia&#39;, &#39;Qual o nome do orgão sensorial das cobras&#39;, &#39;Fosseta loreal&#39;)
-    14 criaCartão (
-    &#39;História&#39;, &#39;Em qual ano acabou a 2° guerra mundial?&#39;, &#39;1945&#39;)
-    15 criaCartão (
-    &#39;Química&#39;, &#39;Qual o menor átomo?&#39;, &#39;Hidrogénio&#39;)
-    16 criaCartão (
-    &#39;Empreendedorismo&#39;, &#39;Diga uma matriz se decisão&#39;, &#39;PDCA&#39;)
-    17 criaCartão (
-    &#39;Geografia&#39;, &#39;Qual a maior potência mundial atualmente?&#39;, &#39;Estados Unidos&#39;)
-    18 criaCartão (
-    &#39;Matemática&#39;, &#39;Qual a fórmula para calcular a área de um triângulo?&#39;, &#39;Base x
-    Altura ÷ 2&#39;)
-    19 criaCartão (
-    &#39;História&#39;, &#39;Em que ano o Brasil foi descoberto&#39;, &#39;1500&#39;)
-    20 criaCartão (
-    &#39;Biologia&#39;, &#39;Como se representão os genes?&#39;, &#39;-A- dominante -a- recessivo&#39;)
+criaCartão(
+'Química',
+'Qual o símbolo químico do Carbono?',
+'C')
+
+criaCartão(
+'Artes',
+'Em que ano se deu início ao período barroco?',
+'1600')
+
+criaCartão (
+'Matemática',
+'Qual a soma dos ângulos de um triângulo?',
+'180° graus')
+
+criaCartão (
+'Biologia',
+'O que é um ser pluricelular?',
+'Um ser com mais de uma célula')
+
+criaCartão (
+'História',
+'No Brasil, quem foi o presidente que mais ficou no cargo?',
+'Getúlio Vargas')
+
+criaCartão (
+'Ed. Física',
+'Quando o vôlei foi criado?',
+'1895')
+
+criaCartão (
+'Inglês',
+'Como se diz OI em inglês?',
+'Hi')
+
+criaCartão (
+'Geografia',
+'Qual o maior país do mundo?',
+'Rússia')
+
+criaCartão (
+'Robótica',
+'Para que servem os jumpers?',
+'Fazer conexões entre o sistema')
+
+criaCartão (
+'Programação',
+'Diga um site que usamos para programar sites',
+'Github')
+
+criaCartão (
+'Filosofia',
+'Diga um filósofo conhecido',
+'Aristóteles')
+
+criaCartão (
+'Sociologia',
+'Quem é o pai da sociologia?',
+'Auguste Comte')
+
+criaCartão (
+'Biologia',
+'Qual o nome do órgão sensorial das cobras?',
+'Fosseta loreal')
+
+criaCartão (
+'História',
+'Em qual ano acabou a 2° guerra mundial?',
+'1945')
+
+criaCartão (
+'Química',
+'Qual o menor átomo?',
+'Hidrogênio')
+
+criaCartão (
+'Empreendedorismo',
+'Diga uma matriz de decisão',
+'PDCA')
+
+criaCartão (
+'Geografia',
+'Qual a maior potência mundial atualmente?',
+'Estados Unidos')
+
+criaCartão (
+'Matemática',
+'Qual a fórmula para calcular a área de um triângulo?',
+'Base x Altura ÷ 2')
+
+criaCartão (
+'História',
+'Em que ano o Brasil foi descoberto?',
+'1500')
+
+criaCartão (
+'Biologia',
+'Como se representam os genes?',
+'-A- dominante -a- recessivo')
