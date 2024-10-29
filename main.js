04c5b7df7351812e61bbcb5d31322b46b1d710a9
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-29 08:49:31 -0300

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criaCartão(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao= document.createElement('article');
     cartao.className = 'cartao';
