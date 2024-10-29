ef1de5a6492da2f58d663b93f17cd3c272e21e9a
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-29 08:49:17 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -1,99 +1,99 @@
-criaCartão(
+criaCartao(
 'Química',
 'Qual o símbolo químico do Carbono?',
 'C')
 
-criaCartão(
+criaCartao(
 'Artes',
 'Em que ano se deu início ao período barroco?',
 '1600')
 
-criaCartão (
+criaCartao (
 'Matemática',
 'Qual a soma dos ângulos de um triângulo?',
 '180° graus')
 
-criaCartão (
+criaCartao (
 'Biologia',
 'O que é um ser pluricelular?',
 'Um ser com mais de uma célula')
 
-criaCartão (
+criaCartao (
 'História',
 'No Brasil, quem foi o presidente que mais ficou no cargo?',
 'Getúlio Vargas')
 
-criaCartão (
+criaCartao (
 'Ed. Física',
 'Quando o vôlei foi criado?',
 '1895')
 
-criaCartão (
+criaCartao (
 'Inglês',
 'Como se diz OI em inglês?',
 'Hi')
 
-criaCartão (
+criaCartao (
 'Geografia',
 'Qual o maior país do mundo?',
 'Rússia')
 
-criaCartão (
+criaCartao (
 'Robótica',
 'Para que servem os jumpers?',
 'Fazer conexões entre o sistema')
 
-criaCartão (
+criaCartao (
 'Programação',
 'Diga um site que usamos para programar sites',
 'Github')
 
-criaCartão (
+criaCartao (
 'Filosofia',
 'Diga um filósofo conhecido',
 'Aristóteles')
 
-criaCartão (
+criaCartao (
 'Sociologia',
 'Quem é o pai da sociologia?',
 'Auguste Comte')
 
-criaCartão (
+criaCartao (
 'Biologia',
 'Qual o nome do órgão sensorial das cobras?',
 'Fosseta loreal')
 
-criaCartão (
+criaCartao (
 'História',
 'Em qual ano acabou a 2° guerra mundial?',
 '1945')
 
-criaCartão (
+criaCartao (
 'Química',
 'Qual o menor átomo?',
 'Hidrogênio')
 
-criaCartão (
+criaCartao (
 'Empreendedorismo',
 'Diga uma matriz de decisão',
 'PDCA')
 
-criaCartão (
+criaCartao (
 'Geografia',
 'Qual a maior potência mundial atualmente?',
 'Estados Unidos')
 
-criaCartão (
+criaCartao (
 'Matemática',
 'Qual a fórmula para calcular a área de um triângulo?',
 'Base x Altura ÷ 2')
 
-criaCartão (
+criaCartao (
 'História',
 'Em que ano o Brasil foi descoberto?',
 '1500')
 
-criaCartão (
+criaCartao (
 'Biologia',
 'Como se representam os genes?',
 '-A- dominante -a- recessivo')
